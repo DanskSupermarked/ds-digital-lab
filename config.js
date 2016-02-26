ec4f3618b5d117db121d7db540628b5e62304f9f
Jakob Viskum Damgaard <jakob.viskum.damgaard@gmail.com>
2016-02-26 19:58:15 +0100

    Mail and API config

--- config.js
+++ config.js
@@ -15,7 +15,16 @@ config = {
     // Configure your URL and mail settings here
     production: {
         url: 'https://ds-digital-lab.azurewebsites.net/',
-        mail: {},
+        mail: {
+            transport: 'SMTP',
+            options: {
+                service: 'Mailgun',
+                auth: {
+                    user: 'postmaster@sandbox6a50bb194e3a44ddbee8ef64b2a10a75.mailgun.org',
+                    pass: 'e8d617484963e983aad61421dca5ac71'
+                }
+            }
+        },
         database: {
             client: 'sqlite3',
             connection: {
@@ -73,76 +82,6 @@ config = {
         paths: {
             contentPath: path.join(__dirname, '/content/')
         }
-    },
-
-    // **Developers only need to edit below here**
-
-    // ### Testing
-    // Used when developing Ghost to run tests and check the health of Ghost
-    // Uses a different port number
-    testing: {
-        url: 'http://127.0.0.1:2369',
-        database: {
-            client: 'sqlite3',
-            connection: {
-                filename: path.join(__dirname, '/content/data/ghost-test.db')
-            },
-            pool: {
-                afterCreate: function(conn, done) {
-                    conn.run('PRAGMA synchronous=OFF;' +
-                        'PRAGMA journal_mode=MEMORY;' +
-                        'PRAGMA locking_mode=EXCLUSIVE;' +
-                        'BEGIN EXCLUSIVE; COMMIT;', done);
-                }
-            }
-        },
-        server: {
-            host: '127.0.0.1',
-            port: '2369'
-        },
-        logging: false
-    },
-
-    // ### Testing MySQL
-    // Used by Travis - Automated testing run through GitHub
-    'testing-mysql': {
-        url: 'http://127.0.0.1:2369',
-        database: {
-            client: 'mysql',
-            connection: {
-                host: '127.0.0.1',
-                user: 'root',
-                password: '',
-                database: 'ghost_testing',
-                charset: 'utf8'
-            }
-        },
-        server: {
-            host: '127.0.0.1',
-            port: '2369'
-        },
-        logging: false
-    },
-
-    // ### Testing pg
-    // Used by Travis - Automated testing run through GitHub
-    'testing-pg': {
-        url: 'http://127.0.0.1:2369',
-        database: {
-            client: 'pg',
-            connection: {
-                host: '127.0.0.1',
-                user: 'postgres',
-                password: '',
-                database: 'ghost_testing',
-                charset: 'utf8'
-            }
-        },
-        server: {
-            host: '127.0.0.1',
-            port: '2369'
-        },
-        logging: false
     }
 };
 
