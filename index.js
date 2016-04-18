885b3cfb4a8494d6c2903643d82b596085cb1ad8
Jakob Viskum Damgaard <jakob.viskum.damgaard@gmail.com>
2016-04-18 13:57:45 +0200

    Update Ghost to 0.7.9

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // # Ghost Startup
 // Orchestrates the startup of Ghost when run from command line.
 var express,
-	ghost,
-	parentApp,
-	errors;
+    ghost,
+    parentApp,
+    errors;
 
 // Make sure dependencies are installed and file system permissions are correct.
 require('./core/server/utils/startup-check').check();
@@ -17,12 +17,12 @@ errors = require('./core/server/errors');
 parentApp = express();
 
 // Call Ghost to get an instance of GhostServer
-ghost().then(function(ghostServer) {
-	// Mount our Ghost instance on our desired subdirectory path if it exists.
-	parentApp.use(ghostServer.config.paths.subdir, ghostServer.rootApp);
+ghost().then(function (ghostServer) {
+    // Mount our Ghost instance on our desired subdirectory path if it exists.
+    parentApp.use(ghostServer.config.paths.subdir, ghostServer.rootApp);
 
-	// Let Ghost handle starting our server instance.
-	ghostServer.start(parentApp);
-}).catch(function(err) {
-	errors.logErrorAndExit(err, err.context, err.help);
+    // Let Ghost handle starting our server instance.
+    ghostServer.start(parentApp);
+}).catch(function (err) {
+    errors.logErrorAndExit(err, err.context, err.help);
 });
