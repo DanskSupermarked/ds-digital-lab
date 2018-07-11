db5662b8d7e20a15224f02519a2bfa5d0407b817
Jesper Brink <jeb.its@dsg.dk>
2018-07-11 11:14:05 +0200

    Adding new banner styling-image and adjusting nav

--- config.js
+++ config.js
@@ -42,7 +42,7 @@ var config = {
     development: {
         // The url to use when providing links to the site, E.g. in RSS and email.
         // Change this to your Ghost blog's published URL.
-        url: 'http://localhost:3000',
+        url: 'http://localhost:2368',
 
         // Example mail config
         // Visit http://support.ghost.org/mail for instructions
@@ -74,7 +74,7 @@ var config = {
             // Host to be passed to node's `net.Server#listen()`
             host: '127.0.0.1',
             // Port to be passed to node's `net.Server#listen()`, for iisnode set this to `process.env.PORT`
-            port: '3000'
+            port: '2368'
         },
         // #### Paths
         // Specify where your content directory lives
